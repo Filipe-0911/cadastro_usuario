b3ab9924335d45151a09e745e1d99c6099392785
Filipe <fbianchi.andrade@gmail.com>
2024-01-08 16:51:39 -0300

    att requirements.txt

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ variavel = Usuario("NOME USUARIO", "CPF ou CNPJ", "CEP", "EMAIL", "TELEFONE")
 """
 
 usuario = Usuario("Filipe", 44060631884, 71060639, "fbianchi.andrade@gmail.com", 5592984171672, "09/11/1993")
-print(usuario)
+print(usuario.idade())
