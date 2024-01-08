0b9d6e09b5e8816c23fa1c24411380475e839ccb
Filipe <fbianchi.andrade@gmail.com>
2024-01-08 16:29:04 -0300

    adicionado atributo data de nascimento em usuario e metodo idade()

--- classes/Usuario.py
+++ classes/Usuario.py
@@ -3,18 +3,25 @@ from classes.Acesso_cep import BuscaEndereco
 from classes.Email import Email
 from classes.Data import Datas
 from classes.TelefonesBr import TelefonesBr
+from datetime import datetime
 
 class Usuario:
-    def __init__(self, nome, documento, cep, email, telefone) -> None:
-        self.nome = nome
-        self.documento = Documento.cria_documento(documento)
-        self.cep = BuscaEndereco(cep)
-        self.email = Email(email)
-        self.data = Datas()
-        self.telefone = TelefonesBr(telefone)
+    def __init__(self, nome, documento, cep, email, telefone, data_nascimento=None) -> None:
+        self._nome = nome
+        self._documento = Documento.cria_documento(documento)
+        self._cep = BuscaEndereco(cep)
+        self._email = Email(email)
+        self._data = Datas()
+        self._telefone = TelefonesBr(telefone)
+        self._data_nascimento = data_nascimento
         
     def detalhar(self):
         return self.__dict__
+    
+    def idade(self):
+        data_atual = datetime.today()
+        data_nascimento = datetime.strptime(self._data_nascimento, '%d/%m/%Y')
+        return ((data_atual - data_nascimento).days) // 365
     
     def __str__(self) -> str:
         return self._format()
@@ -23,4 +30,4 @@ class Usuario:
         return self.detalhar()
     
     def _format(self) -> str:
-        return f"Nome: {self.nome}, Documento: {self.documento}, Email: {self.email}, Data de cadastro: {self.data}, Telefone: {self.telefone}, CEP: {self.cep}"
+        return f"Nome: {self._nome}, Documento: {self._documento}, Email: {self._email}, Data de cadastro: {self._data}, Telefone: {self._telefone}, CEP: {self._cep}, Idade: {self.idade()}"
--- main.py
+++ main.py
@@ -4,3 +4,6 @@ from classes.Usuario import Usuario
 Para criar um novo usuário crie uma variável instanciando a classe Usuario da seguinte maneira:
 variavel = Usuario("NOME USUARIO", "CPF ou CNPJ", "CEP", "EMAIL", "TELEFONE")
 """
+
+usuario = Usuario("Filipe", 44060631884, 71060639, "fbianchi.andrade@gmail.com", 5592984171672, "09/11/1993")
+print(usuario)
